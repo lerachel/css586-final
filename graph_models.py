852cf5eb9eb16f647a9caaa77461ef02abd4c038
chip-kirchner <42825876+chip-kirchner@users.noreply.github.com>
2021-06-01 18:40:24 -0700

    Create graph_models.py
    file to save models used in graph NN

---
+++ graph_models.py
@@ -0,0 +1,33 @@
+import dgl
+import dgl.function as fn
+import dgl.nn as dglnn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class HeteroDotProductPredictor(nn.Module):
+    def forward(self, graph, h, etype):
+        # h contains the node representations for each node type computed from
+        # the GNN defined in the previous section (Section 5.1).
+        with graph.local_scope():
+            graph.ndata['h'] = h
+            graph.apply_edges(fn.u_dot_v('h', 'h', 'score'), etype=etype)
+            return graph.edges[etype].data['score']
+
+class RGCN(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats, rel_names):
+        super().__init__()
+
+        self.conv1 = dglnn.HeteroGraphConv({
+            rel: dglnn.GraphConv(in_feats, hid_feats)
+            for rel in rel_names}, aggregate='sum')
+        self.conv2 = dglnn.HeteroGraphConv({
+            rel: dglnn.GraphConv(hid_feats, out_feats)
+            for rel in rel_names}, aggregate='sum')
+
+    def forward(self, graph, inputs):
+        # inputs are features of nodes
+        h = self.conv1(graph, inputs)
+        h = {k: F.leaky_relu(v) for k, v in h.items()}
+        h = self.conv2(graph, h)
+        return h
