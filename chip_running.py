e0c919cde702d36e50ca311eb8f113f75ca3d87e
chip-kirchner <42825876+chip-kirchner@users.noreply.github.com>
2021-06-08 16:44:01 -0700

    Update chip_running.py
    final model

--- chip_running.py
+++ chip_running.py
@@ -1,3 +1,11 @@
+'''
+chip_running.py
+3/4 Python files submitted for CSS 586 final
+Chip Kirchner
+6/8/2021
+
+Functions below written by Chip Kirchner unless otherwise noted.
+'''
 import scipy.io
 import urllib.request
 import dgl
@@ -10,54 +18,56 @@ import dgl.function as fn
 import dgl.nn as dglnn
 from sklearn.metrics import roc_auc_score, roc_curve
 from sklearn.model_selection import train_test_split
-from torch.utils.tensorboard import SummaryWriter
 from matplotlib import pyplot as plt
 import projectUtils
 import graph_models
 import json
 
-def construct_negative_graph(graph, k, etype):
-    utype, _, vtype = etype
-    src, dst = graph.edges(etype=etype)
-    neg_src = src.repeat_interleave(k)
-    neg_dst = torch.randint(0, graph.num_nodes(vtype), (len(src) * k,))
-    return dgl.heterograph(
-        {etype: (neg_src, neg_dst)},
-        num_nodes_dict={ntype: graph.num_nodes(ntype) for ntype in graph.ntypes})
-
+#function to compute ROC curve
+#written jointly by Chip Kirchner and Rachel Le
 def compute_roc_curve(pos_score, neg_score):
     scores = torch.cat([pos_score.view(-1,), neg_score.view(-1,)]).numpy()
     labels = torch.cat(
         [torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])]).numpy()
     return roc_curve(labels, scores, pos_label=1)
 
+#main model for running graph NN
+#jointly adapted by Chip Kirchner and Rachel Le from Deep Graph Library
+#Reference: https://docs.dgl.ai/en/0.6.x/guide/training-link.html
 class Model(nn.Module):
     def __init__(self, G, in_features, hidden_features, out_features):
         super().__init__()
+        #GNN embedding step
         self.sage = graph_models.RGCN(in_features, hidden_features, out_features, G.etypes)
+        #link prediction step
         self.pred = graph_models.HeteroDotProductPredictor()
 
+        #embedd features of knowledge graph
         embed_dict = {ntype : nn.Parameter(torch.Tensor(G.number_of_nodes(ntype), in_features))
                       for ntype in G.ntypes}
         for key, embed in embed_dict.items():
             nn.init.xavier_uniform_(embed)
         self.embed = nn.ParameterDict(embed_dict)
 
+    #make the embedding and prediction in each forward pass
     def forward(self, g, neg_g, etype):
         h = self.sage(g, self.embed)
         return self.pred(g, h, etype), self.pred(neg_g, h, etype)
 
+    #loss computed using BCE 
     def compute_loss(self, pos_score, neg_score):
         scores = torch.cat([pos_score.view(-1,), neg_score.view(-1,)])
         labels = torch.cat([torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])])
         return F.binary_cross_entropy_with_logits(scores, labels)
 
+    #similar compute the AUC for use in training eval
     def compute_auc(self, pos_score, neg_score):
         scores = torch.cat([pos_score.view(-1,), neg_score.view(-1,)]).numpy()
         labels = torch.cat(
             [torch.ones(pos_score.shape[0]), torch.zeros(neg_score.shape[0])]).numpy()
         return roc_auc_score(labels, scores)
 
+    #train the model, k is size of negative graph compared to positive graph
     def fit(self,opt,graph,test_graph,k=1,num_epochs=5):
         test_losses = []
         train_losses = []
@@ -65,15 +75,18 @@ class Model(nn.Module):
         train_aucs = []
 
         for epoch in range(num_epochs):
+            #get negative samples
             negative_graph = projectUtils.construct_negative_graph(graph, k, ('drug', 'treats', 'disease'))
+            #score the positive and negative samples
             pos_score, neg_score = model(graph, negative_graph, ('drug', 'treats', 'disease'))
+            #compute loss
             loss = self.compute_loss(pos_score, neg_score)
             opt.zero_grad()
             loss.backward()
             train_loss = loss.item()
             opt.step()
             with torch.no_grad():
-                #predit on test graph
+                #predict on test graph
                 neg_test_graph = projectUtils.construct_negative_graph(test_graph, k, ('drug', 'treats', 'disease'))
                 test_pos, test_neg = self(test_graph, neg_test_graph, ('drug', 'treats', 'disease'))
                 test_loss = self.compute_loss(test_pos, test_neg)
@@ -83,17 +96,13 @@ class Model(nn.Module):
                 if epoch % 5 ==0:
                     print('Epoch %d/%d -- Training Loss: %0.3f, Test Loss: %0.3f,AUC: %0.3f' % (epoch,num_epochs,train_loss, test_loss.item(), auc))
 
-            writer.add_scalar('Loss/train', train_loss, epoch)
             train_losses.append(train_loss)
-            writer.add_scalar('AUC/train', train_auc, epoch)
             train_aucs.append(train_auc)
-            writer.add_scalar('Loss/test', test_loss.item(), epoch)
             test_losses.append(test_loss.item())
-            writer.add_scalar('AUC/test', auc, epoch)
             test_aucs.append(auc)
-
         return train_losses, test_losses, train_aucs, test_aucs
 
+#model to test MLP performance adpated from above by Chip Kirchner
 class ModelMLP(Model):
     def __init__(self, G, in_features, hidden_features, out_features):
         super().__init__(G, in_features, hidden_features, out_features)
@@ -106,6 +115,7 @@ class ModelMLP(Model):
             nn.init.xavier_uniform_(embed)
         self.embed = nn.ParameterDict(embed_dict)
 
+#model to test 3 convlution performance adpated from above by Chip Kirchner
 class Model3Conv(Model):
     def __init__(self, G, in_features, hidden_features, out_features):
         super().__init__(G, in_features, hidden_features, out_features)
@@ -118,6 +128,7 @@ class Model3Conv(Model):
             nn.init.xavier_uniform_(embed)
         self.embed = nn.ParameterDict(embed_dict)
 
+#model to test 5 convlution performance adpated from above by Chip Kirchner
 class Model5Conv(Model):
     def __init__(self, G, in_features, hidden_features, out_features):
         super().__init__(G, in_features, hidden_features, out_features)
@@ -130,6 +141,7 @@ class Model5Conv(Model):
             nn.init.xavier_uniform_(embed)
         self.embed = nn.ParameterDict(embed_dict)
 
+#model to test graphSAGE performance adpated from above by Chip Kirchner
 class ModelSAGE(Model):
     def __init__(self, G, in_features, hidden_features, out_features):
         super().__init__(G, in_features, hidden_features, out_features)
@@ -142,6 +154,7 @@ class ModelSAGE(Model):
             nn.init.xavier_uniform_(embed)
         self.embed = nn.ParameterDict(embed_dict)
 
+#model to test graphSAGE mixed with GCN performance adpated from above by Chip Kirchner
 class ModelMix(Model):
     def __init__(self, G, in_features, hidden_features, out_features):
         super().__init__(G, in_features, hidden_features, out_features)
@@ -155,22 +168,26 @@ class ModelMix(Model):
         self.embed = nn.ParameterDict(embed_dict)
 
 
-writer = SummaryWriter()
+#set num epochs used for evaluation and hyper-parametersearch here
+n_epochs = 300
 
-n_epochs = 400
-
+#get data from build graph
 diseaseDrug, diseasePathway, diseaseGene, maps = projectUtils.buildGraph()
 
+#set as correct data types for dgl and torch
 tmp = diseaseDrug.astype('int64')
 diseasePathway = diseasePathway.astype('int64')
 diseaseGene = diseaseGene.astype('int64')
 
 ## train test split here
+## build a train and test graph using random connections in edge list
 train, test = train_test_split(tmp, test_size = 0.2, random_state = 12)
 pathTrain, pathTest = train_test_split(diseasePathway, test_size = 0.2, random_state = 7)
 geneTrain, geneTest = train_test_split(diseaseGene, test_size = 0.2, random_state = 3)
 
 #make two graphs, one for test one for training (and more for comparison)
+#graphs in DGL must be bidirectional for edge propogation
+#drug-treats-diease is the same edge list as diseas-treated-by-drug
 full_graph = dgl.heterograph({
     ('drug', 'treats', 'disease') : (torch.tensor(train[:,1]),torch.tensor(train[:,0])),
     ('disease', 'treated-by', 'drug') : (torch.tensor(train[:,0]),torch.tensor(train[:,1])),
@@ -189,13 +206,19 @@ full_test_graph = dgl.heterograph({
     ('disease', 'relates', 'gene') : (torch.tensor(geneTest[:,0]),torch.tensor(geneTest[:,1]))
 })
 
+#get two subsets of large graph for graph size testing
 DCh_graph = full_graph.edge_type_subgraph(["treats","treated-by"])
 DCh_test_graph = full_test_graph.edge_type_subgraph(["treats","treated-by"])
 
 DChG_graph = full_graph.edge_type_subgraph(["treats","treated-by","related","relates"])
 DChG_test_graph = full_test_graph.edge_type_subgraph(["treats","treated-by","related","relates"])
 
-
+### COMPARING MODELS ###
+'''
+Basic steps are similar through out.
+Create the model using a 15,15,20 structure
+Train the model
+run model on test graph and get AUC and training loss data
 '''
 ## Begin number of convolutions comparison ##
 
@@ -232,6 +255,7 @@ with torch.no_grad():
     Conv5fpr, Conv5tpr, Conv5thresholds = compute_roc_curve(test_pos, test_neg)
     Conv5auc = model.compute_auc(test_pos, test_neg)
 
+#plot the convolution comparisons
 fig = plt.figure()
 lw = 2
 plt.plot(Conv2fpr, Conv2tpr, color='darkorange',
@@ -246,7 +270,6 @@ plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.legend(loc="lower right")
-plt.show() 
 
 fig.savefig('ROC_Convolutions.png',dpi=400,format='png')
 
@@ -264,7 +287,6 @@ plt.plot(Conv5test_losses, color='gray', lw=lw,label='5 Conv Validation Loss',li
 plt.xlabel('Epochs')
 plt.ylabel('Binary Cross Entropy Loss')
 plt.legend(loc="center right")
-plt.show()
 
 fig.savefig('loss_Convolutions.png',dpi=400,format='png')
 
@@ -292,6 +314,7 @@ with torch.no_grad():
     mixfpr, mixtpr, mixthresholds = compute_roc_curve(test_pos, test_neg)
     mixauc = model.compute_auc(test_pos, test_neg)
 
+#plot the model type comparisons
 fig = plt.figure()
 lw = 2
 plt.plot(Conv3fpr, Conv3tpr, color='darkorange',
@@ -306,7 +329,6 @@ plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.legend(loc="lower right")
-plt.show() 
 
 fig.savefig('ROC_Models.png',dpi=400,format='png')
 
@@ -324,7 +346,6 @@ plt.plot(mixtest_losses, color='gray', lw=lw,label='Mix Validation Loss',linesty
 plt.xlabel('Epochs')
 plt.ylabel('Binary Cross Entropy Loss')
 plt.legend(loc="center right")
-plt.show()
 
 fig.savefig('loss_Models.png',dpi=400,format='png')
 
@@ -363,7 +384,7 @@ with torch.no_grad():
     DChGfpr, DChGtpr, DChGthresholds = compute_roc_curve(test_pos, test_neg)
     DChGauc = model.compute_auc(test_pos, test_neg)
 
-
+#plot the graph size comparisons
 fig = plt.figure()
 lw = 2
 plt.plot(fpr, tpr, color='darkorange',
@@ -377,8 +398,7 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.legend(loc="lower right")
-plt.show() 
+plt.legend(loc="lower right") 
 
 fig.savefig('ROC_GraphSize.png',dpi=400,format='png')
 
@@ -396,13 +416,11 @@ plt.plot(DChGtest_losses, color='gray', lw=lw,label='Disease-Drug-Gene Validatio
 plt.xlabel('Epochs')
 plt.ylabel('Binary Cross Entropy Loss')
 plt.legend(loc="center right")
-plt.show()
 
 fig.savefig('loss_GraphSize.png',dpi=400,format='png')
 
 
 ## Begin MLP Consine Similarity Comparison ## 
-
 model = Model(DCh_graph,15, 15, 20)
 optimizer = torch.optim.Adam(model.parameters(),lr=0.001, weight_decay=5e-4)
 DOTtrain_losses, DOTtest_losses, DOTtrain_aucs, DOTtest_aucs = model.fit(optimizer,DCh_graph,DCh_test_graph,num_epochs=n_epochs)
@@ -423,7 +441,7 @@ with torch.no_grad():
     MLPfpr, MLPtpr, MLP50thresholds = compute_roc_curve(test_pos, test_neg)
     MLPauc = model.compute_auc(test_pos, test_neg)
 
-
+#plot the MLP dot product comparison
 fig = plt.figure()
 lw = 2
 plt.plot(DOTfpr, DOTtpr, color='darkorange',
@@ -435,8 +453,7 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.legend(loc="lower right")
-plt.show() 
+plt.legend(loc="lower right") 
 
 fig.savefig('ROC_MLP_DOT.png',dpi=400,format='png')
 
@@ -451,14 +468,11 @@ plt.plot(Conv2test_losses, color='olive', lw=lw,label='MLP Validation Loss',line
 plt.xlabel('Epochs')
 plt.ylabel('Binary Cross Entropy Loss')
 plt.legend(loc="center right")
-plt.show()
 
 fig.savefig('loss_MLP_DOT.png',dpi=400,format='png')
-'''
 
 
 ## Begin Model Grid Search ##
-
 embedded_space = [15,30,100]
 hidden_layers = [5,50,100]
 latent_space = [5,20,50,100]
@@ -468,6 +482,7 @@ results = {}
 n_epochs = 300
 
 i=0
+#manual grid search here to control the order and approach
 for types in prediction:
     for spc in embedded_space:
         for layers in hidden_layers:
@@ -506,9 +521,32 @@ for types in prediction:
                 }
                 i+=1
 
+#save everything to a dictionary and read with the post-processing file provided
 with open("results.json", "w") as outfile: 
     json.dump(results, outfile)
 
+## TRAIN AND RUN FINAL MODEL ##
+# Best inputs from hyper parameter search is a 30, 30, 100 architecture w/ dot product predictor
+model = Model(DCh_graph,30, 100, 100)
+optimizer = torch.optim.Adam(model.parameters(),lr=0.001, weight_decay=5e-4)
+train_losses, test_losses, train_aucs, test_aucs = model.fit(optimizer,DCh_graph,DCh_test_graph,num_epochs=500)
+with torch.no_grad():
+    neg_test_graph = projectUtils.construct_negative_graph(DCh_test_graph, 1, ('drug', 'treats', 'disease'))
+    test_pos, test_neg = model(DCh_test_graph, neg_test_graph, ('drug', 'treats', 'disease'))
 
-writer.flush()
-writer.close()
+    fpr, tpr, thresholds = compute_roc_curve(test_pos, test_neg)
+    auc = model.compute_auc(test_pos, test_neg)
+
+#plot the final graph training and AUC score curves
+fig = plt.figure()
+lw = 2
+plt.plot(train_losses, color='darkorange',
+         lw=lw, label='Training Loss',alpha=0.5)
+plt.plot(test_losses, color='darkorange', lw=lw,label='Validation Loss',linestyle='--')
+plt.plot(test_aucs, color='black',
+         lw=lw, label='AUC Score (final score = %0.3f'%(auc))
+plt.xlabel('Epochs')
+plt.ylabel('Binary Cross Entropy Loss or AUC Score')
+plt.legend(loc="center right")
+
+fig.savefig('final_model.png',dpi=400,format='png')
